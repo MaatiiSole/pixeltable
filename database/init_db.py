6200b6b972e08a17453fea49c2cd6322a8fae6ad
MatiasSole_ <matisole123@gmail.com>
2025-09-13 14:05:15 -0300

    init database

---
+++ database/init_db.py
@@ -0,0 +1,18 @@
+import pixeltable as pxt
+
+# Directorio de la base de datos
+pxt.drop_dir("pixeltable_db", force=True)
+pxt.create_dir("pixeltable_db")
+
+# Estrucura de la base de datos
+docs = pxt.create_table('pixeltable_db.docs', {
+    'filename': pxt.String,
+    'content': pxt.Document
+})
+
+docs.insert([
+    {'filename': 'codigo_penal_juvenil_libro_1', 'content': 'docs/codigo_penal_juvenil_libro_1.pdf'},
+    {'filename': 'codigo_penal_libro_1', 'content': 'docs/codigo_penal_libro_1.pdf'}
+])
+
+print(docs.collect())
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Inicializar la base local
+python database\init_db.py
